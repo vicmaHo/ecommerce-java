89ded1bb0614e20a294c7ed6dc035b7a0a584272
Victor Hernandez <victormanu968@gmail.com>
2026-07-30 23:09:51 -0500

    add: microservicio de clientes

---
+++ microservices/customer-microservice/src/main/java/com/vicma/micorservices/customer_microservice/CustomerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vicma.micorservices.customer_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ microservices/customer-microservice/src/test/java/com/vicma/micorservices/customer_microservice/CustomerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicma.micorservices.customer_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
