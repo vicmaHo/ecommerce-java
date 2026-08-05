691319ecbabf7ea5c67dff29b47a1399305cf4e5
Victor Hernandez <victormanu968@gmail.com>
2026-08-04 22:08:37 -0500

    feat(product-service): estructura inicial de microservicio producto

---
+++ microservices/product-microservice/src/test/java/com/vicma/microservices/product_microservice/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicma.microservices.product_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
