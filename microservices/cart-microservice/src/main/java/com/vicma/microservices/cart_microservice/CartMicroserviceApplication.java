8c97ee9a3298c9c16b526815cd2d35fa870ac587
Victor Hernandez <victormanu968@gmail.com>
2026-08-12 21:45:26 -0500

    chore(cart-service): creacion y configuracion inicial del microservicio

---
+++ microservices/cart-microservice/src/main/java/com/vicma/microservices/cart_microservice/CartMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vicma.microservices.cart_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartMicroserviceApplication.class, args);
+	}
+
+}
